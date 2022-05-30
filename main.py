ddb433035331c25290ddb6ed26744ef36f658ca0
ValeriaMol <106487538+ValeriaMol@users.noreply.github.com>
2022-05-31 02:40:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -443,13 +443,13 @@ def genre(message):
                 slogan = movie[6]
                 director = movie[7]
                 skript = movie[8]
-                photo = movie[9]
+                # photo = movie[9]
                 description = movie[10]
                 link = movie[11]
-        img = open(photo, 'rb')
+        # img = open(photo, 'rb')
         bot.send_message(message.chat.id, f"И фильмом дня становится... *{name}* !!!!!", parse_mode="Markdown") 
         bot.send_message(message.chat.id, f"*Год производства* - {year}\n*Страна* - {country}\n*Жанр* - {genre}\n*Слоган* - {slogan}\n*Режиссер* - {director}\n*Сценарий* - {skript}\n", parse_mode="Markdown")
-        bot.send_photo(message.chat.id, img)
+        # bot.send_photo(message.chat.id, img)
         markup = types.InlineKeyboardMarkup(row_width=1)
         item1 = types.InlineKeyboardButton("Перейти к просмотру фильма", link)
         markup.add(item1)
